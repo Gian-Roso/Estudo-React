f33279b4cf30bc552f58667206b03c7985624f27
Gian <gianoficial305@gmail.com>
2025-04-28 15:25:01 -0300

    ADD/UPDATE:memo:: Adicção de CSS usando global e tambem Modul

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HelloWord from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
 
@@ -9,12 +10,15 @@ function App() {
 
   return (
     <div className="App"> 
+      <Frase/>
       <HelloWord/>
       <SayMyName nome="Gian"/>
       {/* Aqui eu adiciono a função criada, junto a propriedade que eu quero chamar, e então defino o valor dela, isso torna a função dinamica*/}
       <SayMyName nome="Matheus"/>
       <SayMyName nome={nome}/>
       {/* também posso usar uma variavel para passar um dado dinamico pela props */}
+
+      <h1>Hello Word</h1>
 
       <Pessoa 
         nome="Gian"  
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,8 +1,11 @@
+import styles from './Frase.module.css'
+// esse style e semelhante ao props, é um objeto que condensou a estilização e permite que eu acesse  por meio dele as estilizações
+
 function Frase() {
 
     return (
-        <div>
-            <p>Este é um componente com uma frase</p>
+        <div className={styles.fraseContainer }>
+            <p className={styles.fraseContent}>Este é um componente com uma frase</p>
         </div>
     )
 }
