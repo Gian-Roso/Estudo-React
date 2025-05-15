b92b7928b50dd243f6f0132b7ae3d39fc9b9db36
Gian <gianoficial305@gmail.com>
2025-05-14 21:04:42 -0300

    Feat: Router e icons

--- src/App.js
+++ src/App.js
@@ -1,63 +1,21 @@
-import './App.css';
-import HelloWord from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
-import Evento from './components/Evento';
-import Form from './components/Form';
-import Condicional from './components/Condicional';
-import OutraLista from './components/OutraLista';
-import { useState } from 'react';
-import SeuNome from './components/SeuNome';
-import Saldacao from './components/Saldacao';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
+import Empresa from './Pages/Empresa';
+import Contato from './Pages/Contato';
+import NavBar from "./components/Layout/NavBar";
+import Footer from "./components/Layout/Footer";
 
 function App() {
-
-  const nome = "João"
-
-  const meuItens = ['React', 'Vue', 'Angular']
-
-  const [name, setName] = useState()
-
   return (
-    
-    <div className="App"> 
-
-      <h1>State lift</h1>
-      <SeuNome setName={setName}/>
-      <Saldacao name={name}/>
-
-
-      <h1>Renderização de listas</h1>
-      <OutraLista Itens={meuItens}/>
-      <OutraLista Itens={[]}/>
-      <Condicional/>
-
-      <Form/>
-
-      <Evento numero="1"/>
-      
-
-
-      <List/>
-      <Frase/>
-      <HelloWord/>
-      <SayMyName nome="Gian"/>
-      {/* Aqui eu adiciono a função criada, junto a propriedade que eu quero chamar, e então defino o valor dela, isso torna a função dinamica*/}
-      <SayMyName nome="Matheus"/>
-      <SayMyName nome={nome}/>
-      {/* também posso usar uma variavel para passar um dado dinamico pela props */}
-
-      <h1>Hello Word</h1>
-
-      <Pessoa 
-        nome="Gian"  
-        idade="20" 
-        profissao="Nenhuma" 
-        foto="https://via.placeholder.com/150"
-      />
-    </div>
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/empresa" element={<Empresa />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return(
+        <div>
+            <h1>Home</h1>
+            <p>Conteudo da paigna</p>
+        </div>
+    )
+}
+export default Home
